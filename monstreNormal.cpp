4bc01cefcb9fb870918c9bc3b2bcc405f913d746
Nils <72404130+nils-demougeot@users.noreply.github.com>
2026-04-08 05:47:07 +0200

    correctif

--- monstreNormal.cpp
+++ monstreNormal.cpp
@@ -1,7 +1,6 @@
 #include "MonstreNormal.h"
 
-// Le constructeur appelle celui de la classe mère (Monstre)
 MonstreNormal::MonstreNormal(string nom, int hp, int attaque, int defense, int mercyObjectif)
     : Monstre(nom, "NORMAL", hp, attaque, defense, mercyObjectif) {}
-// NORMAL : 2 actions [cite: 44]
+// NORMAL : 2 actions
 int MonstreNormal::getNbActionsActDisponibles() const { return 2; } 
--- monstreboss.cpp
+++ monstreboss.cpp
@@ -3,5 +3,5 @@
 
 MonstreBoss::MonstreBoss(string nom, int hp, int attaque, int defense, int mercyObjectif)
     : Monstre(nom, "BOSS", hp, attaque, defense, mercyObjectif) {}
-// BOSS : 4 actions [cite: 45]
+// BOSS : 4 actions
 int MonstreBoss::getNbActionsActDisponibles() const { return 4; }
